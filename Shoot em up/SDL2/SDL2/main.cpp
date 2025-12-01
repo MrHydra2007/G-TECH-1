30a133943c2f648acb649af7a4a84f057a8c3c11
Iker COT VAQUERO <icotvaquero@gaming.tech>
2025-12-01 11:53:51 +0100

    Premiers exos shoot'em up

---
+++ Shoot em up/SDL2/SDL2/main.cpp
@@ -0,0 +1,109 @@
+#include <SDL.h>
+#include <iostream>
+
+
+
+void DrawHorizontalLine(SDL_Renderer* renderer, int x, int y, int length)
+{
+	SDL_SetRenderDrawColor(renderer,0 , 255, 0, 255);
+	for (int i = 0; i < length; ++i)
+	{
+		SDL_RenderDrawPoint(renderer, x + i, y);
+	}
+}
+
+
+void DrawVerticalLine(SDL_Renderer* renderer, int x, int y, int length)
+{
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	for (int i = 0; i < length; ++i)
+	{
+		SDL_RenderDrawPoint(renderer, x, y + i);
+	}
+}
+
+
+/*void DrawLine(SDL_Renderer* renderer, int x1, int y1, int x2, int y2)
+{
+	int diffx = x2 - x1;
+	int diffy = y2 - y1;
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+	if (diffx < 0)
+	{
+		for (int i = diffx; i != 0; ++i)
+		{
+			SDL_RenderDrawPoint(renderer, x1, y1);
+		}
+	}
+}
+*/
+void DrawSquare(SDL_Renderer* renderer, int x1, int y1, int x2, int y2)
+{
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	for (int i = x1; i < x2; ++i)
+	{
+		for (int p = y1; p < y2; ++p)
+		{
+			SDL_RenderDrawPoint(renderer, i, p); 
+		}
+	}
+}
+
+void DrawRectangle(SDL_Renderer* renderer, int x, int y, int width, int height)
+{
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+	SDL_RenderDrawLine(renderer, x, y, x + width, y);
+	SDL_RenderDrawLine(renderer, x, y + height, x + width, y + height);
+	SDL_RenderDrawLine(renderer, x, y, x, y + height);
+	SDL_RenderDrawLine(renderer, x + width, y, x + width, y + height);
+}
+
+
+void DrawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius, int precision)
+{
+	SDL_SetRenderDrawColor(renderer, 25, 185, 96, 255);
+	
+}
+
+
+void Draw(SDL_Renderer* renderer)
+{
+	DrawSquare(renderer, 20, 20, 100, 100);
+	DrawHorizontalLine(renderer, 200, 300, 50);
+	DrawVerticalLine(renderer, 500, 10, 50);
+	//DrawLine(renderer, 20, 300, 50);
+	DrawRectangle(renderer, 100, 300, 400, 100);
+	SDL_RenderPresent(renderer);
+}
+
+
+int main(int argc, char* argv[])
+{
+		if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		{
+			std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+			return 1;
+		}
+
+		SDL_Window* window = SDL_CreateWindow("SDL", 100, 100, 640, 480, SDL_WINDOW_RESIZABLE);
+
+		SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);;
+
+
+		if (!window)
+		{
+			std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+			SDL_Quit();
+			return 1;
+		}
+
+	Draw(renderer);
+	SDL_Delay(2000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
