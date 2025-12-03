5eb9f34dd88b640c462cd6c64a96d2e5c33ff8bf
Iker COT VAQUERO <icotvaquero@gaming.tech>
2025-12-03 17:40:15 +0100

    update

--- Shoot em up/Project1/Project1/Image.cpp
+++ Shoot em up/Project1/Project1/Image.cpp
@@ -1,11 +1,12 @@
 #include "Image.h"
+#include<iostream>
 
 void Image::Draw(SDL_Renderer* renderer)
 {
     SDL_Surface* image = SDL_LoadBMP("C:/Users/icotvaquero/Documents/image.bmp");
     if (!image)
     {
-        printf("Erreur de chargement de l'image : %s", SDL_GetError());
+        std::cout<<"Erreur de chargement de l'image.";
         return;
     }
     SDL_Texture* monImage = SDL_CreateTextureFromSurface(renderer, image);
